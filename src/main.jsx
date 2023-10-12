d1619b8b50e4d647ec103db351f08ff8f0e5a77d
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-12 09:12:30 +0600

    fetch data from server and display

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Phones from './Components/Phones';
+import Main from './Components/Main';
+import Phone from './Components/Phone';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+        loader: () => fetch('http://localhost:3000/phones'),
+      },
+      {
+        path: "/phone/:id",
+        element: <Phone></Phone>,
+        loader: ({ params }) => fetch(`http://localhost:3000/phones/${params.id}`),
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
